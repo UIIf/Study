894904095c7aa499c33236fda3996e7540f2ee36
Uiif <uiif.github@gmail.com>
2022-04-23 16:53:54 +1000

    ML task

--- VichMat/practics/simpleIterat.py
+++ VichMat/practics/simpleIterat.py
@@ -10,17 +10,25 @@ from matrix import *
 # 	[0.7,1,2,1,0.7,4,3.2]
 # ]
 # b = [3.2, 4.3, 0.1, 3.5, 5.3, 9, 3.7]
-matrix = [
-	[10,2,1],
-	[1,10,2],
-	[1,1,10]
-]
-b = [10,12,8]
 
-def simple_iter_calculate_x(alpha,beta,x):
+matrix = [
+	[16, 2, 0, -2],
+	[4, 20, 1, 0],
+	[2, 0, 10, 0],
+	[-4, 0, 4, 32]
+]
+b = [-13,24,7,0]
+# matrix = [
+# 	[10,2,1],
+# 	[1,10,2],
+# 	[1,1,10]
+# ]
+# b = [10,12,8]
+
+def simple_iter_calculate_x(alpha,beta,x,w):
 	return plus(beta,mul(transpose(dot(alpha,x))[0],-1))
 
-def zaenbal_calculate_x(alpha,beta,x):
+def zaiendal_calculate_x(alpha,beta,x,w):
 	x = x[:]
 	for i in range(len(x)):
 		x[i] = beta[i]
@@ -29,27 +37,44 @@ def zaenbal_calculate_x(alpha,beta,x):
 				x[i] -= alpha[i][j]*x[j]
 	return x
 
-def solve(matrix, b, epsilon, func = simple_iter_calculate_x):
+def relax_calculate_x(alpha,beta,x,w):
+	x = x[:]
+	for i in range(len(x)):
+		x[i] = (1- w)*x[i] + w*beta[i]
+		for j in range(len(x)):
+			if(j != i):
+				x[i] -= w*alpha[i][j]*x[j]
+	return x
+
+def solve(matrix, b, epsilon, func = simple_iter_calculate_x, w = 1):
 	x_prev = [0 for i in range(len(matrix))]
 	alpha = [ [matrix[i][j]/matrix[i][i] for j in range(len(matrix))] for i in range(len(matrix))]
 	for i in range(len(matrix)):
 		alpha[i][i] = 0
 	beta = [b[i]/matrix[i][i] for i in range(len(b))]
 	count = 1
-	x = func(alpha,beta,x_prev)
-	print("B", beta)
-	print("A")
-	for i in alpha:
-		print(i)
+	x = func(alpha,beta,x_prev,w)
+	# print("B", beta)
+	# print("A")
+	# for i in alpha:
+	# 	print(i)
 	print()
 	while(vector_norm_1(plus(mul(x,-1), x_prev)) >= epsilon):
 		x_prev = x
-		x = func(alpha,beta,x_prev)
-		print(x)
+		x = func(alpha,beta,x_prev,w)
+		# print(x)
 		count +=1
 	print(count)
 	return x
 
-x = solve(matrix,b,0.0001, zaenbal_calculate_x)
+x = solve(matrix,b,10**(-10), simple_iter_calculate_x)
 print(x)
 check_x(matrix,b,x)
+x = solve(matrix,b,10**(-10), zaiendal_calculate_x)
+print(x)
+check_x(matrix,b,x)
+x = solve(matrix,b,10**(-10), relax_calculate_x, 0.95)
+print(x)
+check_x(matrix,b,x)
+for i in range(0, 20):
+	solve(matrix,b,10**(-10), relax_calculate_x, i/10)
