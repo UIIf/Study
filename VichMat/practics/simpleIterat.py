a90eed1a55416f69a186a1c643048133592f8b7f
Uiif <uiif.github@gmail.com>
2022-05-04 20:20:58 +1000

    commit kursach

--- VichMat/practics/simpleIterat.py
+++ VichMat/practics/simpleIterat.py
@@ -12,12 +12,12 @@ from matrix import *
 # b = [3.2, 4.3, 0.1, 3.5, 5.3, 9, 3.7]
 
 matrix = [
-	[16, 2, 0, -2],
-	[4, 20, 1, 0],
-	[2, 0, 10, 0],
-	[-4, 0, 4, 32]
+	[16, 2, 1, -2],
+	[4, 20, 1, 5],
+	[2, 1, 10, 3],
+	[-4, 7, 4, 32]
 ]
-b = [-13,24,7,0]
+b = [-13,24,7,2]
 # matrix = [
 # 	[10,2,1],
 # 	[1,10,2],
@@ -46,6 +46,16 @@ def relax_calculate_x(alpha,beta,x,w):
 				x[i] -= w*alpha[i][j]*x[j]
 	return x
 
+def grad_spusk(alpha, beta, x, w):
+	r = plus(b, mul(transpose(dot(matrix,x))[0], -1))
+	# print(F)
+	delta = dot(r,r)[0][0]
+	delta /= dot(r, transpose(dot(matrix,r))[0])[0][0]
+	# print(delta)
+	new_x = plus(x, mul(r, delta))
+	return new_x
+	# return new_x
+
 def solve(matrix, b, epsilon, func = simple_iter_calculate_x, w = 1):
 	x_prev = [0 for i in range(len(matrix))]
 	alpha = [ [matrix[i][j]/matrix[i][i] for j in range(len(matrix))] for i in range(len(matrix))]
@@ -62,19 +72,24 @@ def solve(matrix, b, epsilon, func = simple_iter_calculate_x, w = 1):
 	while(vector_norm_1(plus(mul(x,-1), x_prev)) >= epsilon):
 		x_prev = x
 		x = func(alpha,beta,x_prev,w)
-		# print(x)
+		# print(count,x)
 		count +=1
 	print(count)
 	return x
 
-x = solve(matrix,b,10**(-10), simple_iter_calculate_x)
+# x = solve(matrix,b,10**(-10), simple_iter_calculate_x)
+# print(x)
+# check_x(matrix,b,x)
+# x = solve(matrix,b,10**(-10), zaiendal_calculate_x)
+# print(x)
+# check_x(matrix,b,x)
+# x = solve(matrix,b,10**(-10), relax_calculate_x, 0.95)
+# print(x)
+# check_x(matrix,b,x)
+# for i in range(0, 20):
+# 	solve(matrix,b,10**(-10), relax_calculate_x, i/10)
+
+print(grad_spusk(matrix,b,[0 for i in b], 0))
+x = solve(matrix,b,10**(-3), grad_spusk)
 print(x)
 check_x(matrix,b,x)
-x = solve(matrix,b,10**(-10), zaiendal_calculate_x)
-print(x)
-check_x(matrix,b,x)
-x = solve(matrix,b,10**(-10), relax_calculate_x, 0.95)
-print(x)
-check_x(matrix,b,x)
-for i in range(0, 20):
-	solve(matrix,b,10**(-10), relax_calculate_x, i/10)
