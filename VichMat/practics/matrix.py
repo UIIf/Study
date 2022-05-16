34ee8f495fea67c3ed4603f3b999697dc3233ae1
Uiif <uiif.github@gmail.com>
2022-05-16 12:27:11 +1000

    New Commit

--- VichMat/practics/matrix.py
+++ VichMat/practics/matrix.py
@@ -202,3 +202,68 @@ def vector_norm_1(vec):
 		else:
 			vec = vec[0]
 	return sum([abs(x) for x in vec])
+
+
+
+	#---------------------------------------------------------------------
+	#Собственные значения матрицы
+
+def sgn(num):
+	return 1 if num > 0 else -1
+
+def find_max_nd_ind(matrix):
+	mi, mj = 0,1
+	for i in range(len(matrix)):
+		for j in range(len(matrix[i])):
+			if(i != j and abs(matrix[i][j]) > abs(matrix[mi][mj])):
+				mi, mj = i, j
+	return [mi, mj]
+
+def find_max_diag_ind(matrix):
+	mi = 0
+	for i in range(1, len(matrix)):
+		if(matrix[i][i] > matrix[mi][mi]):
+			mi = i
+	return mi
+
+def self_mean_method_sim(matrix, p):
+	tau = [10**(-(i+1)) for i in range(p)]
+
+	matrix = [r[:] for r in matrix]
+	counter = 0
+	for p in tau:
+		i, j = find_max_nd_ind(matrix)
+
+		diag_i = find_max_diag_ind(matrix)
+		while(abs(matrix[i][j]) >= p): #abs(matrix[diag_i][diag_i])**0.5 * 
+			d = ((matrix[i][i] - matrix[j][j])**2 + 4*(matrix[i][j])**2)**0.5
+			c = (0.5*( 1 + abs(matrix[i][i] - matrix[j][j])/d))**0.5
+			s = sgn(matrix[i][j]*(matrix[i][i] - matrix[j][j])) * (0.5*(1 - abs(matrix[i][i] - matrix[j][j])/d))**0.5
+
+			new_m = [ r[:] for r in matrix]
+
+			new_m[i][j], new_m[j][i] = 0, 0
+
+			for k in range(len(matrix)):
+				if(k == i or k == j):
+					continue
+				new_m[i][k] = c * matrix[k][i] + s* matrix[k][j]
+				new_m[k][i] = new_m[i][k]
+
+				new_m[j][k] = -s * matrix[k][i] + c* matrix[k][j]
+				new_m[k][j] = new_m[j][k]
+			
+			new_m[i][i] = c *  c * matrix[i][i] + 2 * c*s * matrix[i][j] + s*s *matrix[j][j]
+
+			new_m[j][j] = s * s * matrix[i][i] - 2 * c * s * matrix[i][j] + c * c * matrix[j][j]
+
+			matrix = new_m
+
+			i, j = find_max_nd_ind(matrix)
+			diag_i = find_max_diag_ind(matrix)
+			counter += 1
+	# print(counter)
+	return [matrix[i][i] for i in range(len(matrix))]
+
+	#/Собственные значения матрицы
+	#---------------------------------------------------------------------
--- VichMat/practics/simpleIterat.py
+++ VichMat/practics/simpleIterat.py
@@ -1,4 +1,10 @@
 from matrix import *
+from math import log, log10, cos, pi
+class Method:
+	def __init__(self, func, change_matrix = True, w = 1):
+		self.func = func
+		self.ch = change_matrix
+		self.w = w
 
 # matrix = [
 # 	[2.2,4,-3,1.5,0.6,2,0.7],
@@ -11,13 +17,13 @@ from matrix import *
 # ]
 # b = [3.2, 4.3, 0.1, 3.5, 5.3, 9, 3.7]
 
-matrix = [
-	[16, 2, 1, -2],
-	[4, 20, 1, 5],
-	[2, 1, 10, 3],
-	[-4, 7, 4, 32]
-]
-b = [-13,24,7,2]
+# matrix = [
+# 	[16, 2, 1, -2],
+# 	[4, 20, 1, 5],
+# 	[2, 1, 10, 3],
+# 	[-4, 7, 4, 32]
+# ]
+# b = [-13,24,7,2]
 # matrix = [
 # 	[10,2,1],
 # 	[1,10,2],
@@ -25,8 +31,16 @@ b = [-13,24,7,2]
 # ]
 # b = [10,12,8]
 
+matrix = [
+	[2 , 1  , 1],
+	[1 , 2.5, 1],
+	[1 , 1  , 3],
+]
+
+b = [1, 1, 1]
+
 def simple_iter_calculate_x(alpha,beta,x,w):
-	return plus(beta,mul(transpose(dot(alpha,x))[0],-1))
+	return plus(beta,mul(tranвspose(dot(alpha,x))[0],-1))
 
 def zaiendal_calculate_x(alpha,beta,x,w):
 	x = x[:]
@@ -54,42 +68,64 @@ def grad_spusk(alpha, beta, x, w):
 	# print(delta)
 	new_x = plus(x, mul(r, delta))
 	return new_x
-	# return new_x
 
-def solve(matrix, b, epsilon, func = simple_iter_calculate_x, w = 1):
+def Richardson(alpha, beta, x, n):
+	
+	lambdas = self_mean_method_sim(alpha, 11)
+	# print(int(log10(eps)))
+	new_x = x[:]
+	lmax = max(lambdas)
+	lmin = min(lambdas)
+
+	tau_z = 2/(lmin + lmax)
+	eta = lmin/lmax
+	ro = (1 - eta)/(1 + eta)
+
+	ro_one = (1 - eta**0.5)/(1 + eta**0.5)
+	for k in range(1, n+1):
+
+		nu = cos((2*k-1)*pi/2/n)
+		tau = tau_z/(1 + ro * nu)
+
+		new_x = plus(mul(plus(beta, transpose(mul(dot(alpha, new_x), -1))[0]),tau),new_x)
+
+	return new_x
+		
+
+def solve(matrix, b, epsilon, meth):
 	x_prev = [0 for i in range(len(matrix))]
-	alpha = [ [matrix[i][j]/matrix[i][i] for j in range(len(matrix))] for i in range(len(matrix))]
-	for i in range(len(matrix)):
-		alpha[i][i] = 0
-	beta = [b[i]/matrix[i][i] for i in range(len(b))]
+
+	if(meth.ch):
+
+		alpha = [ [matrix[i][j]/matrix[i][i] for j in range(len(matrix))] for i in range(len(matrix))]
+		for i in range(len(matrix)):
+			alpha[i][i] = 0
+		beta = [b[i]/matrix[i][i] for i in range(len(b))]
+	else:
+		alpha = [ r[:] for r in matrix]
+		beta = b[:]
+
 	count = 1
-	x = func(alpha,beta,x_prev,w)
-	# print("B", beta)
-	# print("A")
-	# for i in alpha:
-	# 	print(i)
-	print()
+	x = meth.func(alpha, beta, x_prev, meth.w)
 	while(vector_norm_1(plus(mul(x,-1), x_prev)) >= epsilon):
 		x_prev = x
-		x = func(alpha,beta,x_prev,w)
+		x = meth.func(alpha,beta,x_prev, meth.w)
+		# print(count,x)
 		# print(count,x)
 		count +=1
-	print(count)
+	print('Count:',count)
 	return x
 
-# x = solve(matrix,b,10**(-10), simple_iter_calculate_x)
-# print(x)
-# check_x(matrix,b,x)
-# x = solve(matrix,b,10**(-10), zaiendal_calculate_x)
-# print(x)
-# check_x(matrix,b,x)
-# x = solve(matrix,b,10**(-10), relax_calculate_x, 0.95)
-# print(x)
-# check_x(matrix,b,x)
-# for i in range(0, 20):
-# 	solve(matrix,b,10**(-10), relax_calculate_x, i/10)
+eps = 1e-2
 
-print(grad_spusk(matrix,b,[0 for i in b], 0))
-x = solve(matrix,b,10**(-3), grad_spusk)
-print(x)
+grad = Method(grad_spusk, False)
+simpl = Method(simple_iter_calculate_x)
+zedal = Method(zaiendal_calculate_x)
+relax = Method(relax_calculate_x, w = 1)
+richard = Method(Richardson,False, w = 6)
+
+# x = solve(matrix, b, eps, zedal)
+
+x = solve(matrix, b, eps, richard)
+print('X', x)
 check_x(matrix,b,x)
